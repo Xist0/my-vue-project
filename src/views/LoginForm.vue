45885acc081a8ae8387f5990da9b6659ce0a25eb
Xist0 <x1spridcherd@gmail.com>
2023-09-19 18:25:23 +0500

    1

--- src/views/LoginForm.vue
+++ src/views/LoginForm.vue
@@ -1,46 +1,65 @@
 <template>
   <form class="card auth-card" @submit.prevent="submitHandler">
-    <!-- ... -->
-    <div class="input-field">
-      <input
-        id="email"
-        type="text"
-        v-model="email"
-        :class="{ invalid: v.email.$invalid }"
-      />
+    <div class="card-content">
+      <span class="card-title">Домашняя бухгалтерия</span>
+      <div class="input-field">
+        <input
+          id="login"
+          type="text"
+          v-model="v.login"
+          :class="{ invalid: v.login?.$error }"
+        />
 
-      <label for="email">Логин</label>
+        <label for="login">Логин</label>
 
-      <small
-        class="helper-text invalid"
-        v-for="(error, index) of v.email.$errors"
-        :key="index"
-      >
-        {{ printError(error.$validator, error.$params) }}
-      </small>
+        <small
+          class="helper-text invalid"
+          v-for="(error, key) in v.login?.$errors"
+          :key="key"
+        >
+          {{ printError(key, error.$params) }}
+        </small>
+      </div>
+      <div class="input-field">
+        <input id="password" type="password" v-model="v.password" />
+        <label for="password">Пароль</label>
+        <small class="helper-text invalid">Password</small>
+      </div>
     </div>
-    <!-- ... -->
+    <div class="card-action">
+      <div>
+        <button class="btn waves-effect waves-light auth-submit" type="submit">
+          Войти
+          <i class="material-icons right">send</i>
+        </button>
+      </div>
+
+      <p class="center">
+        Нет аккаунта?
+        <router-link to="/register">Зарегистрироваться</router-link>
+      </p>
+    </div>
   </form>
 </template>
 
 <script>
-import router from "@/router";
+
 import { useVuelidate } from "@vuelidate/core";
-import { required, minLength, email } from "@vuelidate/validators";
+import { required, minLength } from "@vuelidate/validators";
+import router from "@/router";
 
 export default {
   name: "LoginForm",
-
   setup() {
     const v = useVuelidate({
-      email: { email, required },
+      login: { required, minLength: minLength(3) },
       password: { required, minLength: minLength(6) },
     });
 
     const submitHandler = () => {
-      v.$touch();
+      v.value.$touch();
 
-      if (v.$invalid) return;
+      if (v.$error) return;
 
       router.push("/");
     };
@@ -50,14 +69,11 @@ export default {
         return "Поле не должно быть пустым";
       } else if ($name === "minLength") {
         return "Минимальная длина должна быть " + $param + " символов";
-      } else if ($name === "email") {
-        return "Неправильный формат email";
       }
     };
 
     return {
-      email: v.email,
-      password: v.password,
+      v,
       submitHandler,
       printError,
     };
